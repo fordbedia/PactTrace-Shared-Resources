3941bd2176f89c985f164e99e96fe74499ee60b4
Ed Bedia <fordbedia@gmail.com>
2026-08-09 01:05:38 -0500

    implement routes and controller for the Sign Up

--- src/Modules/User/routes/api.php
+++ src/Modules/User/routes/api.php
@@ -1,2 +1,9 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use PactTraceSDK\SharedResources\Modules\User\Http\Controllers\RegistrationController;
+
+Route::group(['prefix' => 'user'], function () {
+	Route::apiResource('register', RegistrationController::class);
+});
